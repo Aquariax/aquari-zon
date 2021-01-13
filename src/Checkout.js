b1eaf02b6e8ba5000de0ee13f32990e4ee3faa98
aquariax <roberthrrs8@gmail.com>
2021-01-13 02:43:59 -0500

    added cart functionality(adding and removing products from basket)

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 function Checkout() {
+	const [{ basket }, dispatch] = useStateValue();
 	return (
 		<div className='checkout'>
 			<div className='checkout_left'>
@@ -11,6 +14,15 @@ function Checkout() {
 					className='checkout_ad'
 				/>
 				<h2 className='checkout_title'>Your Basket</h2>
+				{basket.map((item) => (
+					<CheckoutProduct
+						id={item.id}
+						title={item.title}
+						image={item.image}
+						price={item.price}
+						rating={item.rating}
+					/>
+				))}
 			</div>
 			<div className='checkout_right'>
 				<h2>Total Goes Here</h2>
