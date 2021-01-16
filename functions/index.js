14a1a244080f34b9a603b9c4f75c708835e20280
aquariax <roberthrrs8@gmail.com>
2021-01-16 02:53:26 -0500

    Revert "redeploy for firebase"
    This reverts commit 1c188803321fdec172131c37bd79c49b70f0f535.
    
    "going back to previous commit"

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const cors = require("cors");
-const stripe = require("stripe")("sk_test_51IA0suKjwEXfIyb5mPPIL4ypiiS11yUkvUfkCX0j8TYeMAh3oKUQluOMfyc3OmHOqZtDxRGIGhFnvmUqwtdb3iu300HnivZ0od"
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(
+	'sk_test_51IA0suKjwEXfIyb5mPPIL4ypiiS11yUkvUfkCX0j8TYeMAh3oKUQluOMfyc3OmHOqZtDxRGIGhFnvmUqwtdb3iu300HnivZ0od'
 );
 const app = express();
 
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.get("/", (request, response) => response.status(200).send("hello world"));
-app.post("/payments/create", async (request, response) => {  
-  const total = request.query.total;
-  console.log("payment recieved for:", total);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total,
-    currency: "usd",
-  });
+app.get('/', (request, response) => response.status(200).send('hello world'));
+app.post('/payments/create', async (request, response) => {
+	const total = request.query.total;
+	console.log('payment recieved for:', total);
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: total,
+		currency: 'usd',
+	});
 
 	response.status(201).send({
 		clientSecret: paymentIntent.client_secret,
