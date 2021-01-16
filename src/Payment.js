5975f3b058368f46bb06b1ef4bcaba9ab4d83330
aquariax <roberthrrs8@gmail.com>
2021-01-16 02:13:12 -0500

    added in stripe payment method

--- src/Payment.js
+++ src/Payment.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
 import { getBasketTotal } from './reducer';
+import axios from './axios';
 
 function Payment() {
 	const [{ basket, user }, dispatch] = useStateValue();
 	const [error, setError] = useState(null);
 	const [disabled, setDisabled] = useState(true);
-	const [processing, setProcessing] = useState(false);
-	const [succeeded, setSucceeded] = useState('');
+	const [processing, setProcessing] = useState('');
+	const [succeeded, setSucceeded] = useState(false);
+	const [clientSecret, setClientSecret] = useState(true);
 
 	const stripe = useStripe();
 	const elements = useElements();
+	const history = useHistory();
 
-	const handleSubmit = (e) => {};
+	useEffect(() => {
+		const getClientSecret = async () => {
+			const response = await axios({
+				method: 'post',
+				url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+			});
+			setClientSecret(response.data.clientSecret);
+		};
+
+		getClientSecret();
+	}, [basket]);
+
+	console.log('client secret is >>>', clientSecret);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setProcessing(true);
+
+		const payload = await stripe
+			.confirmCardPayment(clientSecret, {
+				payment_method: {
+					card: elements.getElement(CardElement),
+				},
+			})
+			.then(({ paymentIntent }) => {
+				setSucceeded(true);
+				setError(null);
+				setProcessing(false);
+				history.replace('/orders');
+			});
+	};
+
 	const handleChange = (e) => {
 		setDisabled(e.empty);
 		setError(e.error ? e.error.message : '');
@@ -74,6 +108,7 @@ function Payment() {
 									<span>{processing ? <p>Processing</p> : 'Buy Now'}</span>
 								</button>
 							</div>
+							{error && <div>{error}</div>}
 						</form>
 					</div>
 				</div>
